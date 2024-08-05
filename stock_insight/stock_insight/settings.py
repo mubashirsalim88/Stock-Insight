140ed41029916151dd2c2cb9366b53ccc0a289d3
ved105205 <vedant.nehal@mscds.christuniversity.in>
2024-08-05 17:51:37 +0530

    added templates

--- stock_insight/stock_insight/settings.py
+++ stock_insight/stock_insight/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'stock_insight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 WSGI_APPLICATION = 'stock_insight.wsgi.application'
 
